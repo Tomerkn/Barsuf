c31fa642b4dbdd473f115a4ac30c10bacec89552
Tomer Knobler <tomerknobler@Tomers-MacBook-Air.local>
2026-06-13 13:24:40 +0300

    fix: change Tenders page height to h-full to resolve scrolling conflicts with main layout

--- src/pages/Tenders.jsx
+++ src/pages/Tenders.jsx
@@ -374,7 +374,7 @@ export default function Tenders() {
   }
 
   return (
-    <div className="p-6 flex flex-col gap-6 w-full max-w-[1600px] mx-auto h-[calc(100vh-4rem)] overflow-hidden">
+    <div className="p-6 flex flex-col gap-6 w-full max-w-[1600px] mx-auto h-full overflow-hidden">
       {/* כפתור חזרה וכותרת הדף */}
       <div className="flex flex-col gap-2 shrink-0">
         <div className="flex items-center justify-between">
